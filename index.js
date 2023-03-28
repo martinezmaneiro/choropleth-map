bc7ef4f30d7313a1bdc54657e359ebd72644e908
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-28 06:51:19 -0300

    [ADDED] database and canvas variables

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const educationUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
 const countryUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
 
+let countyData;
+let educationData;
+
+let canvas = d3.select('#canvas');
