56714125eeaadb14c8a86dd1408a3624b5dc59f9
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-28 06:44:52 -0300

    [ADDED] html file structure, js file resources, css default properties reseted

---
+++ index.js
@@ -0,0 +1,3 @@
+const educationUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+const countryUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+
